c5b112dc6b35358a02512b8ff50eaee7904c9463
Thangggg <thang.nguyenvan7594@gmail.com>
2021-11-10 13:48:40 +0700

    circle

---
+++ src/Circle/Main.java
@@ -0,0 +1,13 @@
+package Circle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+
+        circle.setRadius(5.0);
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+    }
+}
