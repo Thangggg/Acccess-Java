f2a93a63283fcb60c4e5bdff93abe5b374995946
Thangggg <thang.nguyenvan7594@gmail.com>
2021-11-10 14:21:54 +0700

    student

---
+++ src/Student/Student.java
@@ -0,0 +1,25 @@
+package Student;
+
+public class Student {
+    private String name = "Thắng";
+    private String classes = "C09";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+}
